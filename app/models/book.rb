432ee40ac44be62ee84e795f7cf520d0c8dc025b
JAJAKIM22 <peterjajakim@gmail.com>
2025-02-19 11:27:00 +0300

    CREATED MODEL VALIDATIONS FOR BOTH BOOK AND BORROWING

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,9 @@
 class Book < ApplicationRecord
+  has_many :borrowings, dependent: :destroy
+  validates :title, :author, :isbn, presence: true
+  validates :isbn, uniqueness: true
+
+  def available?
+    status
+  end
 end
--- app/models/borrowing.rb
+++ app/models/borrowing.rb
@@ -1,4 +1,19 @@
 class Borrowing < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :borrowed_on, :due_on, presence: true
+
+  after_create :mark_book_as_unavailable
+  after_destroy :mark_book_as_available
+
+  private
+
+  def mark_book_as_unavailable
+    book.update(status: false)
+  end
+
+  def mark_book_as_available
+    book.update(status: true)
+  end
 end
